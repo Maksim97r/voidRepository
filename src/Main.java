2ada4dd39f6960be95379a4cf363e05dc7d9aecf
maksim97r <game452@mail.ru>
2024-11-27 16:12:44 +0300

    aaa

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет");
         System.out.println("aaa");
+        System.out.println("sss");
     }
 }
